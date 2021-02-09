2fde2c4a7d433dbce292f8ffdc279b8c140df0c9
Daniel <raiff_@hotmail.com>
2021-02-08 22:19:47 -0500

    better solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {Subscription, interval} from 'rxjs';
+import {createUrlResolverWithoutPackagePrefix} from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -16,18 +17,25 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     const timer = interval(1000);
-    const subscribe = timer.subscribe(x => { console.log(Date()); this.Timer = Date();});
+    const subscribe = timer.subscribe(x => { this.Timer = Date();});
   }
 
   // tslint:disable-next-line:typedef
   CountDown(releaseTime, Timer) {
     // const release = new Date(releaseTime *  1000);
-    // @ts-ignore
-    const calculatedTime = releaseTime - new Date(Timer);
+    const currentTime = new Date(Timer);
 
-    return new Date(calculatedTime);
+    // @ts-ignore
+    let calculatedTime = releaseTime.getTime() - currentTime.getTime();
+    console.log(calculatedTime);
+    const deltaTime = new Date(calculatedTime);
+    const daysCalculated = calculatedTime / (1000 * 3600 * 24);
+    console.log(deltaTime.getHours());
+    console.log(deltaTime.getMinutes());
+    console.log(deltaTime.getSeconds());
+    const val = `Days: ${daysCalculated.toFixed(0)} ${deltaTime.getHours()}:${deltaTime.getMinutes()}:${deltaTime.getSeconds()} `;
+    return val;
   }
 
 }
-
 
