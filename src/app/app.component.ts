1d76eb2a3ca4d2cb9bbe68d25376cb59b1eefc3f
Daniel <raiff_@hotmail.com>
2021-02-08 20:54:15 -0500

    Quick Timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {Subscription, interval, BehaviorSubject} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'countdown';
+export class AppComponent implements OnInit {
+  releaseTimes = [new Date(1613783224 * 1000), new Date(1614474424 * 1000), new Date(1613351224 * 1000)];
+  Timer;
+
+  constructor(private ref: ChangeDetectorRef) {
+  }
+
+
+  ngOnInit(): void {
+    const timer = interval(1000);
+    const subscribe = timer.subscribe(x => { console.log(Date()); this.Timer = Date();});
+  }
+
+  // tslint:disable-next-line:typedef
+  CountDown(releaseTime, Timer) {
+    // const release = new Date(releaseTime *  1000);
+    // @ts-ignore
+    const calculatedTime = releaseTime - new Date(Timer);
+    console.log(typeof(releaseTime));
+
+    return new Date(calculatedTime);
+  }
+
 }
+
+
